c920c8f9002ec8eb5ffb436b6314e948f594e797
Saksham <saksham.8484@gmail.com>
2022-03-21 22:45:42 +0530

    feat: filter by categories done

--- src/redux/product-list-reducer/action.js
+++ src/redux/product-list-reducer/action.js
@@ -1,4 +1,4 @@
-import { filterByKirigami, filterByLayeringArt, filterByOrigami, filterByPaperCuttings, filterByQuilling, filterByshadowBox } from "../../utils/filterByCategory";
+import { filterByKirigami, filterByLayeringArt, filterByMiniatures, filterByOrigami, filterByOrigami3d, filterByPaperCuttings, filterByQuilling, filterByshadowBox } from "../../utils/filterByCategory";
 import { includeFastDelivery } from "../../utils/includeFastDelivery";
 import { includeOutOfStock } from "../../utils/includeOutOfStock";
 import { onlyOnSale } from "../../utils/onlyOnSale";
@@ -64,6 +64,7 @@ export const fastDelivery = (payload) => ({
 
 export const filteredData = (state, data) => {
     let filteredData = [...data].filter(item => item.categoryName === "Product Cart")
+    let reserveData = [...data].filter(item => item.categoryName === "Product Cart")
 
     //SORT BY PRICE
     if(state.sortByPrice === HIGH_TO_LOW) filteredData = highToLow(filteredData);
@@ -79,36 +80,52 @@ export const filteredData = (state, data) => {
 
     //CATEGORIES
     if(state.categories.paperCutting) {
-        const temp = filterByPaperCuttings(filteredData);
-        filteredData = [...filteredData, ...temp]
+        const temp = filterByPaperCuttings(reserveData);
+        if(filteredData.find(item => item.type === "PAPER CUTTING")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
     }
 
     if(state.categories.shadowBox) {
-        const temp = filterByshadowBox(filteredData);
-        filteredData = [...filteredData, ...temp]
+        const temp = filterByshadowBox(reserveData);
+        if(filteredData.find(item => item.type === "SHADOW BOX")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
     }
 
     if(state.categories.quilling) {
-        const temp = filterByQuilling(filteredData);
-        filteredData = [...filteredData, ...temp]
+        const temp = filterByQuilling(reserveData);
+        if(filteredData.find(item => item.type === "QUILLING")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
     }
 
     if(state.categories.layering) {
-        const temp = filterByLayeringArt(filteredData);
-        filteredData = [...filteredData, ...temp]
+        const temp = filterByLayeringArt(reserveData);
+        if(filteredData.find(item => item.type === "LAYERING ART")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
     }
 
     if(state.categories.origami) {
-        const temp = filterByOrigami(filteredData);
-        filteredData = [...filteredData, ...temp]
+        const temp = filterByOrigami(reserveData);
+        if(filteredData.find(item => item.type === "ORIGAMI")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
     }
 
     if(state.categories.kirigami) {
-        const temp = filterByKirigami(filteredData);
-        filteredData = [...filteredData, ...temp]
+        const temp = filterByKirigami(reserveData);
+        if(filteredData.find(item => item.type === "KIRIGAMI")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
     }
 
-    if(state.categories.paperCutting) filteredData = filterByPaperCuttings(filteredData);
+    if(state.categories.origami3d) {
+        const temp = filterByOrigami3d(reserveData);
+        if(filteredData.find(item => item.type === "3D ORIGAMI")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
+    }
+
+    if(state.categories.miniautres) {
+        const temp = filterByMiniatures(reserveData);
+        if(filteredData.find(item => item.type === "MINIATURES")) filteredData = temp
+        else filteredData = [...filteredData, ...temp]
+    }
     
     //RANGE
     filteredData = filteredData.filter(items => Number(items.price) < Number(state.range))
