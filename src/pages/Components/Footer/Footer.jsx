7e0fb4e8bc06f8e0c203693b46667a267d8e1a41
Saksham <55906931+sakshamAK@users.noreply.github.com>
2024-11-13 11:45:03 +0530

    Update Footer.jsx

--- src/pages/Components/Footer/Footer.jsx
+++ src/pages/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <div className={`${styles.flexCont}`}>
       <h2>ARTISAN</h2>
-      <!--<p>&copy;Developed By <a href = "https://sakshamak.netlify.app" target = "_blank" rel = "noreferrer">Saksham</a></p>-->
+      <p>&copy;Developed By <a href = "https://sakshamak.netlify.app" target = "_blank" rel = "noreferrer">Saksham</a></p>
       <ul className = {`${styles.iconList}`}>
         <li><i className = "fa fa-instagram"></i></li>
         <li><i className = "fa fa-twitter"></i></li>
